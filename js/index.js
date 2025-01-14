db1e9dfb6f21bfb695b383e2a5f69f4780d2cb5c
SAMURAItest001 <1013183maple.aws@gmail.com>
2025-01-14 16:05:57 +0900

    1月14日　1on1のため一時データ保存
    index.js、headerについてきく

--- js/index.js
+++ js/index.js
@@ -19,19 +19,26 @@ $(function() {
 
 $(document).ready(function() {
   $('.header_menu a[href^="#"]').click(function(){
+    console.log()
     // 移動先を0px調整する。0を30にすると30px下にずらすことができる。
     var adjust = 0;
+    console.log()
     // スクロールの速度（ミリ秒）
     var speed = 400;
+    console.log()
     // アンカーの値取得 リンク先（href）を取得して、hrefという変数に代入
     var href= $(this).attr("href");
+    console.log()
     // 移動先を取得 リンク先(href）のidがある要素を探して、targetに代入
     var target = $(href == "#" || href == "" ? 'html' : href);
+    console.log()
     // 移動先を調整 idの要素の位置をoffset()で取得して、positionに代入
     var position = target.offset().top + adjust;
+    console.log()
     // スムーススクロール linear（等速） or swing（変速）
     $('body,html').animate({scrollTop:position}, speed, 'swing');
       return false;
+      console.log()
   });
 });
   
