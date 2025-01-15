e8956a3565b6964d575ac3c4cdc7d2ab78147167
SAMURAItest001 <1013183maple.aws@gmail.com>
2025-01-15 22:54:10 +0900

    1月15日　index.jsのスロースクロール、フォームのチェック修正

--- js/index.js
+++ js/index.js
@@ -18,81 +18,73 @@ $(function() {
 });
 
 $(document).ready(function() {
-  $('.header_menu a[href^="#"]').click(function(){
-    console.log()
+  $('.header_menu a[href^="/#"]').click(function(){
     // 移動先を0px調整する。0を30にすると30px下にずらすことができる。
     var adjust = 0;
-    console.log()
     // スクロールの速度（ミリ秒）
     var speed = 400;
-    console.log()
     // アンカーの値取得 リンク先（href）を取得して、hrefという変数に代入
     var href= $(this).attr("href");
-    console.log()
+    href = href.replace("/#", "#");                 // スラッシュを消す！
     // 移動先を取得 リンク先(href）のidがある要素を探して、targetに代入
     var target = $(href == "#" || href == "" ? 'html' : href);
-    console.log()
     // 移動先を調整 idの要素の位置をoffset()で取得して、positionに代入
-    var position = target.offset().top + adjust;
-    console.log()
+    var position = target.offset().top + adjust; 
     // スムーススクロール linear（等速） or swing（変速）
     $('body,html').animate({scrollTop:position}, speed, 'swing');
       return false;
-      console.log()
   });
 });
   
-
-// $('form').submit(function() {
-//   let tel = $('input[type="tel"]').val(); // 【09月26日　課題四で追加】半角スペースを消しました。
-//     if(tel){
-//       const regex = /^0\d{9,10}$/
-//        if(!regex.test(tel)) {
-//          $(".tel_validation").css("display","block");
-//          validation_flg = true
-//     }else
-//     {
-// //エラーじゃないのにメッセージがあったら
-// $(".tel_validation").remove();
-//     }
-//     }
-//     if(validation_flg) {
-//       // return false;
-//    }
-// })
-
 // 名前、席の場所、メールアドレスの必須項目（送信できないようにする）
 $(document).ready(function() {
+  // 初期状態でのバリデーション実行
   validation();
-  $('form input, form select').on('input change',function(){
+
+  // 入力または変更イベントが発生した際にバリデーションを再実行
+  $('form input, form select').on('input change', function() {
     validation();
   });
 
-function validation(){
-  let select = $("select").val();
-  let tel = $('input[name="tel"]').val();
-  let tel_validation = true;
-  if(tel){
-    const regexp = /^0\d{9,10}$/
-       if(!regexp.test(tel)) {
-         $(".tel_validation").css("display","block");
-         tel_validation = false;
-  }
-}
+  function validation() {
+    let name = $('input[name="name"]').val();
+    let email = $('input[name="email"]').val();
+    let seat = $('select[name="seat"]').val();
+    let tel = $('input[name="tel"]').val();
+    let tel_validation = true; // 電話番号のバリデーション用フラグ
 
-  if(
-    $('input[name="name"]').val() !== ""&&
-    $('input[name="email"]').val() !== ""&&
-    $('select[name="seat"]').val() !== ""&&
-    tel_validation
-  ){
-    $('input[name="submit"]').prop("disabled",false);
-    $(".button_submit").css("opacity","1");
-    $(".tel_validation").css("display","none");
+    // 電話番号のバリデーション
+    if (tel) {
+      const tel_regexp = /^0\d{9,10}$/; // 日本の電話番号形式 (0から始まり、10~11桁)
+      if (!tel_regexp.test(tel)) {
+        $(".tel_validation").text("電話番号を正しく入力してください").css("display", "block");
+        tel_validation = false;
+      } else {
+        $(".tel_validation").css("display", "none"); // 正しい場合は非表示
+      }
+    } else {
+      $(".tel_validation").css("display", "none"); // 未入力の場合も非表示
+    }
+
+    // メールアドレスのバリデーション
+    const email_regexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // 一般的なメール形式
+    let email_validation = email_regexp.test(email);
+
+    // 全体のバリデーション判定
+    if (
+      name !== "" && // 名前が入力されているか
+      email !== "" && // メールアドレスが入力されているか
+      email_validation && // メールアドレスが正しい形式か
+      seat !== "" && // 席が選択されているか
+      tel_validation // 電話番号が正しい形式か（または未入力か）
+    ) {
+      // 全ての条件を満たす場合、送信ボタンを有効化
+      $('input[name="submit"]').prop("disabled", false);
+      $(".button_submit").css("opacity", "1");
+    } else {
+      // いずれかの条件を満たさない場合、送信ボタンを無効化
+      $('input[name="submit"]').prop("disabled", true);
+      $(".button_submit").css("opacity", "0.5");
+    }
   }
-  else{
-    $('input[name="submit"]').prop("disabled",true);
-    $(".button_submit").css("opacity","0.5");
-  }
-}
 });
